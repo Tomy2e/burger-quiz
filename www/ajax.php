5736e98456c850ee68843b75d5febec2626feae5
Tomy <tomy.guichard@isen-ouest.yncrea.fr>
2018-06-11 17:24:31 +0200

    Travail sur l'ajax.php (2) Actions 'new_game' et 'next_question' terminées

--- www/ajax.php
+++ www/ajax.php
@@ -51,14 +51,18 @@ if($_GET['action'] == 'new_game')
         $_SESSION['current_proposition_timesent'] = 0;
         $_SESSION['current_score'] = 0;
 
-        print_r($_SESSION);
-
         // OK!!
-        echo "game started";
+        echo json_encode(array(
+            'status' => 'ok'
+        ));
 
     } catch (Exception $e)
     {
         echo $e->getMessage();
+        echo json_encode(array(
+            'status' => 'error',
+            'message' => $e->getMessage()
+        ));
     }
 
 }
@@ -72,11 +76,11 @@ else if($_GET['action'] == 'next_question')
             // It is the first time the user calls "next_question"
             if(is_null($_SESSION['current_propositions']))
             {
-                $_SESSION['current_propositions'] = $currentPartie->getQuestions()[0]->fetchRandomPropositions();
+                $_SESSION['current_propositions'] = serialize($currentPartie->getQuestions()[0]->fetchRandomPropositions());
             }
             else
             {
-                if($_SESSION['current_proposition_index'] == count($_SESSION['current_propositions']) - 1)
+                if($_SESSION['current_proposition_index'] == count(unserialize($_SESSION['current_propositions'])) - 1)
                 {
                     // Go to the next question
                     if($_SESSION['current_question_index'] == count($currentPartie->getQuestions()) - 1)
@@ -86,7 +90,7 @@ else if($_GET['action'] == 'next_question')
                     
                     $_SESSION['current_question_index']++;
                     $_SESSION['current_proposition_index'] = 0;
-                    $_SESSION['current_propositions'] = $currentPartie->getQuestions()[$_SESSION['current_question_index']]->fetchRandomPropositions();
+                    $_SESSION['current_propositions'] = serialize($currentPartie->getQuestions()[$_SESSION['current_question_index']]->fetchRandomPropositions());
                 }
                 else
                 {
@@ -97,7 +101,25 @@ else if($_GET['action'] == 'next_question')
 
             $_SESSION['current_proposition_answered'] = false;
             $_SESSION['current_proposition_timesent'] = time();
-            print_r($_SESSION['current_propositions'][$_SESSION['current_proposition_index']]);
+
+            $currentPropositions = unserialize($_SESSION['current_propositions']);
+
+            $currentProposition = $currentPropositions[$_SESSION['current_proposition_index']];
+            
+            //print_r($currentPartie->getQuestions()[$_SESSION['current_question_index']]);   
+            //print_r($currentProposition);
+
+            $currentQuestion = $currentPartie->getQuestions()[$_SESSION['current_question_index']];
+
+            echo json_encode(array(
+                'status' => 'ok',
+                'current_question' => $currentQuestion->getLibelle1() . ", " . $currentQuestion->getLibelle2() . " ou les deux ?",
+                'current_proposition' => $currentProposition->getLibelle(),
+                'libelle1' => $currentQuestion->getLibelle1(),
+                'libelle2' => $currentQuestion->getLibelle2(),
+                'current_question_progress' => $_SESSION['current_question_index'] + 1,
+                'current_proposition_progress' => $_SESSION['current_proposition_index'] + 1
+            ));
 
         }
         else
@@ -106,6 +128,13 @@ else if($_GET['action'] == 'next_question')
         }        
     } catch(Exception $e)
     {
-        echo $e->getMessage();
-    }
+        echo json_encode(array(
+           'status' => 'error',
+           'message' => $e->getMessage() 
+        ));
+    }    
+}
+else if($_GET['action'] == 'answer_question')
+{
+    
 }
