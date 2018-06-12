e9a77a0dd6c2876b992ff66fbde05661f8e73639
Tomy <tomy.guichard@isen-ouest.yncrea.fr>
2018-06-12 13:51:18 +0200

    Revert ajax.php (fd405bd2588033eb05428b97efcbf38a1e767aa4)

--- www/ajax.php
+++ www/ajax.php
@@ -57,6 +57,8 @@ if($_GET['action'] == 'new_game')
         $_SESSION['current_proposition_answered'] = true;
         $_SESSION['current_proposition_timesent'] = 0;
         $_SESSION['current_score'] = 0;
+        $_SESSION['current_propositions_correct'] = 0;
+        $_SESSION['current_partietime'] = 0;
 
         // OK!!
         echo json_encode(array(
@@ -154,6 +156,8 @@ else if ($_GET['action'] == 'answer_question')
             {
                $answerCorrect = true; 
 
+               $_SESSION['current_propositions_correct']++;
+
                 // Check how much time taken to answer
                 $timeToAnswer = time() - $_SESSION['current_proposition_timesent'];
 
@@ -202,6 +206,7 @@ else if ($_GET['action'] == 'answer_question')
 
 
             $_SESSION['current_score'] += $answerPoints;
+            $_SESSION['current_partietime'] += $timeToAnswer;
             $_SESSION['current_proposition_answered'] = true;
 
             // Check if the game is finished
@@ -211,6 +216,9 @@ else if ($_GET['action'] == 'answer_question')
                 $partieFinished = true;
 
                 // Add score to database
+                $newScore = new Score($_SESSION['current_propositions_correct'], 9,
+                $_SESSION['current_partietime'], $_SESSION['current_score']);
+                $newScore->addInDatabase($user, $currentPartie);
             }
             else
             {
