b2b3550a02190328484d81c108d75e48948ed156
Gwenolé <herrcrazi@gmail.com>
2018-06-12 12:15:54 +0200

    [BKG]Autocommit Blitzkrieg

---
+++ www/templates/footer.php
@@ -0,0 +1,8 @@
+<footer class="hnav">
+	<ul class="hnav-list">
+		<li><span class="legal">© Junk Food Games inc.</span></li>
+		<li><a href="sitemap.php">Plan du site</a></li>
+		<li><a href="legal.html">Mentions légales</a></li>
+		<li><a href="about.php">À propos</a></li>
+	</ul>
+</footer>
---
+++ www/templates/header.php
@@ -0,0 +1,42 @@
+<head>
+	<!--link rel="stylesheet" href="css/header.css"-->
+	<link rel="shortcut icon" href="resources/img/favicon.png">
+
+	<script src="js/header.js"></script>
+	<script src="js/window.js"></script>
+</head>
+
+<header class="hnav hbox">
+
+	<img id="site-logo" src="resources/img/burger-content.png" alt="">
+	<h1>The Ultimate Burger Quiz</h1>
+
+	<div class="spacer"></div>
+
+	<span id="hnav-right">
+		<ul class="hnav-list">
+			<li><a id="nav-menu" href="index.php">Menu</a></li>
+			<li><a id="nav-quiz" href="gamesettings.php">Quiz</a></li>
+			<li><a id="nav-ranking" href="ranking.php">Classements</a></li>
+		</ul>
+
+		<div class="triangle" id="marker"></div>
+	</span>
+
+	<div class="roundel" id="profile-selector">
+		<img src="resources/img/users/vlad.png" alt="vlad" id="profile-pic">
+	</div>
+
+	<div class="window vbox hidden" id="profile-modal">
+		<div class="triangle" id="modal-toparrow"></div>
+
+		<h2>Vlad</h2>
+
+		<div class="roundel centered" id="window-profile-pic">
+			<img src="resources/img/users/vlad.png" alt="vlad" id="profile-pic-big">
+		</div>
+
+		<button id="btn-param">Paramétres</button>
+		<button class="highlighted" id="btn-disconnect">Déconnexion</button>
+	</div>
+</header>
