c12d2aca123b5d0a543468bbc8189a282d9c7e9b
Gwenolé <herrcrazi@gmail.com>
2018-06-13 17:06:00 +0200

    [BKG]Autocommit Blitzkrieg

--- www/settings.php
+++ www/settings.php
@@ -1,4 +1,16 @@
 <!DOCTYPE html>
+
+
+<?php
+require_once('php/autoload.php');
+
+if ( !isConnected($user) )
+{
+	header('Location: connect.php');
+	exit;
+}
+?>
+
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -11,7 +23,7 @@
 		<link rel="stylesheet" type="text/css" href="css/fonts.css">
 		<link rel="stylesheet" type="text/css" href="css/header.css">
 		<link rel="stylesheet" type="text/css" href="css/footer.css">
-		<link rel="stylesheet" type="text/css" href="css/menu.css">
+		<link rel="stylesheet" type="text/css" href="css/settings.css">
 
 		<script src="js/main.js"></script>
 	</head>
@@ -21,7 +33,24 @@
 		<!-- Page content -->
 		<div id="page-settings" class="main-content">
 			<h1>Paramétres</h1>
-			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde modi sapiente voluptatum quo dignissimos consequuntur vitae architecto quas assumenda, saepe quia accusamus quidem in aut eaque odio debitis, ratione a!</p>
+			
+			<div id="settings" class="hbox">
+				<form class="vbox" action="#">
+					<label for="username">Nom d'utilisateur</label>
+					<input type="text" value="<?php echo $user->getUsername(); ?>" name="username">
+
+					<label for="mail">Adresse mail</label>
+					<input type="text" value="<?php echo $user->getEmail(); ?>" name="mail">
+
+					<div id="lbl-changepasswd" class="hbox">
+						<label for="password">Mot de passe</label>
+						<a id="btn-changepasswd" href="#">Modifier le mot de passe</a>
+					</div>
+					<input type="password" disabled value="<?php echo $user->getPassword(); ?>" name="password">
+
+					<button class="highlighted" type="submit">Enregistrer les modifications</button>
+				</form>
+			</div>
 		</div>
 
 		<?php require_once("templates/footer.php"); ?>
