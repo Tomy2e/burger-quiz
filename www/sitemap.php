6ab49109c638610a0a8f3f56e983a841280a406e
Gwenolé <herrcrazi@gmail.com>
2018-06-15 12:23:39 +0200

    Pages de mentions légales, plan du site et page à propos

---
+++ www/sitemap.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta author="Gwenolé Leroy-Ferrec - Tomy Guichard">
+
+		<title>The Ultimate Burger Quiz</title>
+
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" type="text/css" href="css/generic-theme.css">
+		<link rel="stylesheet" type="text/css" href="css/fonts.css">
+		<link rel="stylesheet" type="text/css" href="css/header.css">
+        <link rel="stylesheet" type="text/css" href="css/footer.css">
+        <link rel="stylesheet" type="text/css" href="css/misc.css">
+
+		<script src="js/main.js"></script>
+	</head>
+	<body>
+		<?php require_once("templates/header.php"); ?>
+
+		<!-- Page content -->
+		<div id="page-menu" class="main-content">
+            <h1>Plan du site</h1>
+            
+            <span class="centered">Vous êtes ici --><em id="location">×</em></span>
+		</div>
+
+		<?php require_once("templates/footer.php"); ?>
+	</body>
+</html>
